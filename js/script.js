7820610ecae4324fcfe964067b956afc1287b597
Ewerton Luna <ewerton.al@gmail.com>
2019-08-06 11:59:28 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,35 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+Student: Ewerton Luna
 ******************************************/
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+const pageHeaderDiv = document.querySelector('div.page-header')
 const students = document.getElementsByClassName('student-item');
 const numOfStudentsPerPage = 10;
 
+appendPageLinks(students);
+showPage(students, 1);
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+const searchDiv = createElement('div', 'className', 'student-search');
+const searchInput = createElement('input', 'placeholder', 'Search for students...');
+const searchButton = createElement('button', 'textContent', 'Search');
+searchDiv.appendChild(searchInput);
+searchDiv.appendChild(searchButton);
+pageHeaderDiv.appendChild(searchDiv);
+
+
+
+
+
+function createElement(elementName, property, value){
+    const element = document.createElement(elementName);
+    element[property] = value;
+    return element;
+}
 
 function showPage(liElements, pageNumber){
     const startingIndex = (pageNumber - 1) * numOfStudentsPerPage;
@@ -47,11 +43,6 @@ function showPage(liElements, pageNumber){
     }
 }
 
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
 function appendPageLinks(liElements){
     const numOfLinks = Math.ceil(liElements.length / numOfStudentsPerPage);
     const pageDiv = document.querySelector('div.page');
@@ -74,11 +65,8 @@ function appendPageLinks(liElements){
     pageDiv.appendChild(paginationDiv);
 }
 
-appendPageLinks(students);
-showPage(students, 1);
 
 const linksUl = document.querySelector('div.pagination').firstElementChild;
-
 linksUl.addEventListener('click', (e) => {
     const links = linksUl.children;
     
@@ -93,11 +81,3 @@ linksUl.addEventListener('click', (e) => {
         showPage(students, pageNumber);
     }
 });
-
-
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
