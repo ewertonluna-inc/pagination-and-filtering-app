fa16811b004727fce6e499d8d62cf9f16f823739
Ewerton Luna <ewerton.al@gmail.com>
2019-08-09 11:47:57 -0300

    Finished project
    - Added comments to the functions
    - Wrote README file

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # techdegree_project_2
  My second techdegree project
+
+- This project uses the principle of progressive enhancement to make the webpage more functional.
+- I'm attempting to earn the "Exceed Expectations" grade.
+- It takes the html file that contains several li elements related to students information and adds
+some features to it:
+1) Search box was included
+    1.1) The search input will filter out the student as you type letters into it. You can use either
+    keyup filter or the search button. Both are functional.
+    1.2) If no matching result is found, the page will display a message to inform the user.
+2) Page links are functional it is always updated to the page depending on the number of students 
+    to be displayed.
+    2.1) The max number of students per page link is 10.
--- js/script.js
+++ js/script.js
@@ -10,9 +10,6 @@ const pageHeaderDiv = document.querySelector('div.page-header')
 const students = document.getElementsByClassName('student-item');
 const numOfStudentsPerPage = 10;
 
-appendPageLinks(students);
-showPage(students, 1);
-
 const searchDiv = createElement('div', 'className', 'student-search');
 const searchInput = createElement('input', 'placeholder', 'Search for students...');
 const searchButton = createElement('button', 'textContent', 'Search');
@@ -20,6 +17,10 @@ searchButton.className = 'search-button';
 searchDiv.appendChild(searchInput);
 searchDiv.appendChild(searchButton);
 pageHeaderDiv.appendChild(searchDiv);
+
+// Displaying the page for the first time
+appendPageLinks(students);
+showPage(students, 1);
 
 
 searchButton.addEventListener('click', () => {
@@ -39,6 +40,23 @@ searchButton.addEventListener('click', () => {
 });
 
 
+/* The searchName function 
+- cleans the p.noResultsMessage element if there's one.
+- takes an input element value and li collection
+- for each li in the collection, it tests to see
+if the input value is included in the li element
+witch the student's name.
+    - If the student name includes the input value
+    the li element is pushed to the matchedNames array.
+
+- If the matchedNames array ends up being empty it will
+    - check to see the array is empty because the input 
+    is an empty string.
+        - If so, it will clean p.noResultsMessage element if 
+        there's one.
+    - Otherwise, it will create an p.noResultsMessage and 
+    place it before the ul.student-list element.
+*/
 function searchName(liElements, inputElement){
     cleanNoResulstMessage();
     const matchedNames = [];
@@ -67,6 +85,7 @@ function searchName(liElements, inputElement){
             const noResultsParagraph = createElement('p', 'id', 'noResultsMessage');
             noResultsParagraph.textContent = 'NO MATCHING RESULTS';
             noResultsParagraph.style.color = 'red';
+            noResultsParagraph.style.backgroundColor = 'yellow';
             page.appendChild(noResultsParagraph);
             page.insertBefore(noResultsParagraph, ul);
         }
@@ -90,6 +109,10 @@ searchInput.addEventListener('keyup', () => {
     }
 });
 
+/* The cleanNoResultsMessage function
+- queries for elemenet contaning the noResultsMessage ID
+- if the element exists, it will remove it from the DOM.
+*/
 function cleanNoResulstMessage(){
     const noResultsParagraph = document.getElementById('noResultsMessage');
     if (noResultsParagraph){
@@ -98,12 +121,18 @@ function cleanNoResulstMessage(){
     }
 }
 
+
 function createElement(elementName, property, value){
     const element = document.createElement(elementName);
     element[property] = value;
     return element;
 }
 
+/* The showPage function
+- takes a collection of student li elements and a page number
+- displays the li element if it's index is inside the valid range
+according to the number of students per page.  
+*/
 function showPage(liElements, pageNumber){
     const startingIndex = (pageNumber - 1) * numOfStudentsPerPage;
     const endingIndex = (pageNumber * numOfStudentsPerPage) - 1;
@@ -116,6 +145,27 @@ function showPage(liElements, pageNumber){
     }
 }
 
+
+/* The appendPageLinks function
+- takes a collection of student li elements
+- firstly it will remove any pagination div to
+prevent the script from duplicating page links.
+- Creates a brand new pagination div.
+- Math.ceil is used to calculate the number of links
+because it will always have to round up if the number of
+li elements is not a multiple of the number of students per
+page. Exemple:
+    - If numOfStudentsPerPage is 10 and liElements.length
+    is 11, it will have to create 2 page links. One for the
+    first 10 and another one for the next remaining student.
+- Inside the for loop it will
+    - create the elements to use as links
+    - add the number of the page to the link
+    - append the links to the ul element created inside the function.
+    - make the first link as the active one
+    - append the ul element to the correct location in the DOM (paginationDiv)
+    - Add an event listener for the new created ul.
+*/
 function appendPageLinks(liElements){
     const isTherePaginationDiv = document.querySelector('div.pagination');
     if (isTherePaginationDiv){
@@ -146,6 +196,11 @@ function appendPageLinks(liElements){
 }
 
 
+/*This function takes an ul element, 
+more especifically, the one related to the
+page links and adds the event handler to it.
+The second parameter is necessary in order to display
+the student li items according to which link was clicked */
 function addClickEventListenerToUl(ul, studentLiItems){
     const links = ul.children;
 
